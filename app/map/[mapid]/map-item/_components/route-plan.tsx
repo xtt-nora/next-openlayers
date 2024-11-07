05b99650cce5ae712c77467e2b1f1080d0f015d8
DESKTOP-AMTPMOF\xtt <xutingting7307@163.com>
2024-11-07 19:52:34 +0800

    feat: 路线

--- app/map/[mapid]/map-item/_components/route-plan.tsx
+++ app/map/[mapid]/map-item/_components/route-plan.tsx
@@ -1,4 +1,5 @@
 import { GitBranchPlus, Save } from "lucide-react";
+import { useState } from "react";
 
 export const RoutePlan = () => {
   const list = [
@@ -11,8 +12,46 @@ export const RoutePlan = () => {
       title: "保存数据",
     },
   ];
+  const [routeList, setRouteList] = useState([
+    {
+      routeName: "未知路线",
+      id: 0,
+      isEdit: false,
+      isSelected: true,
+      routerColor: "409EFF",
+      routerGroup: [
+        {
+          name: "TravelNote",
+          point: {},
+          order: 1,
+        },
+        {
+          name: "镰仓",
+          point: {},
+          order: 2,
+        },
+        {
+          name: "秋叶原",
+          point: {},
+          order: 3,
+        },
+        {
+          name: "银座",
+          point: {},
+          order: 4,
+        },
+      ],
+    },
+  ]);
   const clickEvent = (item: string) => {
     console.log(item, "item");
+  };
+  const selectItem = (item: { isSelected: boolean }) => {
+    // routeList.forEach((route) => {
+    //   route.isSelected = false;
+    // });
+    // setRouteList(routeList.map())
+    item.isSelected = true;
   };
   return (
     <div className=" flex flex-col flex-nowrap w-full h-full">
@@ -28,7 +67,15 @@ export const RoutePlan = () => {
           </div>
         ))}
       </div>
-      <div className="w-full h-[calc(100%-40px)] overflow-auto flex flex-col flex-nowrap flex-items-center"></div>
+      <div className="w-full h-[calc(100%-40px)] overflow-auto flex flex-col flex-nowrap flex-items-center">
+        {routeList.map((item, index) => (
+          <div
+            className="w-250px border-b border-solid b-b-blueGray mb-10px"
+            onClick={() => selectItem(item)}
+            key={index}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 };
