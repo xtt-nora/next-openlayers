4f986fa07d9316aebf1199d1130162bf4c3c75ca
DESKTOP-AMTPMOF\xtt <xutingting7307@163.com>
2024-11-13 14:20:57 +0800

    feat: 形成路线连接

--- hooks/useAddEvent.ts
+++ hooks/useAddEvent.ts
@@ -2,8 +2,11 @@ import { useRouteplanModal } from "@/store/use-route-modal";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
-import { Map } from "ol";
-import { Id } from "@/convex/_generated/dataModel";
+import { Feature, Map } from "ol";
+import VectorSource from "ol/source/Vector";
+import { LineString } from "ol/geom";
+import { Stroke, Style } from "ol/style";
+import VectorLayer from "ol/layer/Vector";
 export const useAddEvent = () => {
   const { routeplanId } = useRouteplanModal();
   const { mutate, pending } = useApiMutation(api.routeplan.addSubRoute);
@@ -12,7 +15,7 @@ export const useAddEvent = () => {
     mutate({
       routeplanId: routeplanId,
       name: data[0].name,
-      point: [parseFloat(data[0].lat), parseFloat(data[0].lon)],
+      point: [parseFloat(data[0].lon), parseFloat(data[0].lat)],
       order: Math.random(),
     })
       .then(() => {
@@ -29,11 +32,37 @@ export const useAddEvent = () => {
       isEdit?: boolean;
       isSelected?: boolean;
       routerColor?: string;
-      routerGroup?: { name: string; point: {}; order: number }[];
+      routerGroup?: any;
     },
     map: Map | null
   ) => {
-    console.log(item, map, "createLine");
+    if (!item.routerGroup || !map) return;
+    const coordinates = item.routerGroup?.map((point: { point: any }) => point.point);
+    if (!coordinates) return;
+    console.log("Coordinates in createLineEvent:", coordinates);
+    const route = new LineString(coordinates);
+    const routeFeature = new Feature({
+      type: "route",
+      geometry: route,
+    });
+    routeFeature.setStyle(
+      new Style({
+        stroke: new Stroke({
+          color: item.routerColor || "blue",
+          width: 4,
+        }),
+      })
+    );
+
+    const vectorLayer = new VectorLayer({
+      source: new VectorSource({
+        features: [routeFeature],
+      }),
+    });
+    map.addLayer(vectorLayer);
+    console.log(vectorLayer?.getSource()?.getFeatures());
+    const extent = route.getExtent();
+    map.getView().fit(extent, { padding: [50, 50, 50, 50] });
   };
   return { addEvent, pending, createLineEvent };
 };
