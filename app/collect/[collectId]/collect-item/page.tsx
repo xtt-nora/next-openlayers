25def699ac05ea072cecac46214725862b0191c1
DESKTOP-AMTPMOF\xtt <xutingting7307@163.com>
2024-11-20 19:48:37 +0800

    feat:设计收藏页面

--- app/collect/[collectId]/collect-item/page.tsx
+++ app/collect/[collectId]/collect-item/page.tsx
@@ -8,6 +8,8 @@ import { useSaveEvent } from "@/hooks/useAddEvent";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import { List, Undo2 } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 
 interface CollectParams {
   collectId: Id<"collect">;
@@ -42,12 +44,23 @@ const CollectPage = ({ params }: CollectProps) => {
   const { saveEvent } = useSaveEvent();
   return (
     <div className="w-full h-full flex flex-row">
-      {data && <div className="w-[270px] h-full">{data.collectName}</div>}
+      {data && (
+        <div className="w-[270px] h-full">
+          <div className="mt-4 px-2">
+            <Undo2 size={32} />
+          </div>
+          <Separator className="my-4" />
+          <div className="flex flex-row flex-nowrap px-2">
+            <List />
+            <span className=" text-lg font-black inline-block ml-1">{data.collectName}</span>
+          </div>
+        </div>
+      )}
       <div className="w-[calc(100%-271px)]  h-full relative">
         <div className="absolute top-[10px] left-[10px]  z-[99999] w-30%">
           <SearchInput />
         </div>
-        <ComMap zoom={4} search={search} isadd={false} saveEvent={saveEvent} />
+        {/* <ComMap zoom={4} search={search} isadd={false} saveEvent={saveEvent} /> */}
       </div>
     </div>
   );
